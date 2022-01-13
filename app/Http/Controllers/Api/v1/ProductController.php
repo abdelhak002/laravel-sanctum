524cdf5761672d65423250022d8d2950f64c23cb
abdelhak002 <darbeida.abdelhak@gmail.com>
2022-01-13 02:16:30 +0100

    feat(controller): create product create prodcut request validation and controller with CRUD methods and a search method

---
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\ProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        return Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'slug' => $request->slug,
+            'price' => $request->price,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        return $product->update($request->validated());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json();
+    }
+
+    public function search($name)
+    {
+        return Product::where('name', 'like', '%'. $name .'%')->get();
+    }
+}
